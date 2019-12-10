3bc041f0b50201599b78dbe30152625ca570bbfd
xiangxx <buaaxxx1995@gmail.com>
2019-12-10 19:17:58 +0800

    add required attribute

--- AElfContractDecoder/Models/Base64InfoDto.cs
+++ AElfContractDecoder/Models/Base64InfoDto.cs
@@ -1,7 +1,10 @@
-﻿namespace AElfContractDecoder.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AElfContractDecoder.Models
 {
     public class Base64InfoDto
     {
+        [Required]
         public string Base64String { get; set; }
     }
 }
